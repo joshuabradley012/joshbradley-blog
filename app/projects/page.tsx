37b6ca509e7a8f69be06cef508bd83c835c52403
joshuabradley012 <joshuabradley012@gmail.com>
2025-03-23 20:57:08 -0700

    Finished projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -31,107 +31,167 @@ export default function Projects() {
         of my personal and professional growth to the people I've worked with on
         these teams.
       </p>
-      <div className="mt-12 flex">
-        <Link href="https://celest.tech" target="_blank">
+
+      <div className="mt-16 mb-8 flex">
+        <Link href="https://celest.tech/" target="_blank">
           <Celest className="h-5 text-black" />
         </Link>
       </div>
       <p>
-        I'm building <InlineLink href="https://celest.tech">Celest</InlineLink>{" "}
+        I'm building <InlineLink href="https://celest.tech/">Celest</InlineLink>{" "}
         with my co-founder{" "}
         <InlineLink href="https://www.linkedin.com/in/marco-malacara-b89612152/">
           Marco Malacara
         </InlineLink>
-        .{" "}
+        . We are rebuilding the foundation of Martech on an AI-driven datalayer
+        that will enable smart, personalized content throughout the customer
+        journey.
       </p>
-      <div className="mt-12 flex">
-        <Link href="https://procore.com" target="_blank">
-          <Procore className="h-4 text-black" />
+      <p>
+        My focus is on engineering the core product, tackling the challenge of
+        creating something that both users and AI love to use. We are building
+        using Next.js and Vercel.
+      </p>
+
+      <div className="mt-16 mb-8 flex">
+        <Link href="https://procore.com/" target="_blank">
+          <Procore className="h-3.5 text-black" />
         </Link>
       </div>
-      <div className="mt-12 flex">
+      <p>
+        I worked at <InlineLink href="https://procore.com/">Procore</InlineLink>{" "}
+        for 4 years, learning everything about digital marketing. I worked here
+        pre and post-IPO, experiencing how world class teams scale through
+        hypergrowth.
+      </p>
+      <p>
+        I wore many hats, doing web development, conversion rate optimization,
+        analytics, reporting, paid ads, and SEO. It helped me refine my
+        understanding of the edges of Martech.
+      </p>
+
+      <div className="mt-16 mb-8 flex">
         <Link href="https://procore.com/jobsite" target="_blank">
-          <Jobsite className="h-7 text-black" />
+          <Jobsite className="h-6 text-black" />
         </Link>
       </div>
-      <div className="mt-12 flex">
-        <Link href="https://usesilo.com" target="_blank">
-          <Silo className="h-6 text-black" />
+      <p>
+        I rebuilt{" "}
+        <InlineLink href="https://procore.com/jobsite">Jobsite</InlineLink>,
+        Procore's news website, on a modern, highly optimized, custom WordPress
+        theme. I did this as a part of my efforts to improve SEO.
+      </p>
+
+      <div className="mt-16 mb-8 flex">
+        <Link href="https://usesilo.com/" target="_blank">
+          <Silo className="h-5 text-black" />
         </Link>
       </div>
-      <div className="mt-12 flex">
-        <Link href="https://assignar.com" target="_blank">
+      <p>
+        <InlineLink href="https://usesilo.com/">Silo</InlineLink> was my first
+        client after founding Bradley Digital. I built them a cutting edge
+        website on Next.js to represent their world-class brand and support
+        their quickly growing startup.
+      </p>
+
+      <div className="mt-16 mb-8 flex">
+        <Link href="https://assignar.com/" target="_blank">
           <Assignar className="h-6 text-black" />
         </Link>
       </div>
-      <div className="mt-12 flex">
-        <Link href="https://pacifica.edu" target="_blank">
+      <p>
+        I assisted{" "}
+        <InlineLink href="https://assignar.com/">Assignar</InlineLink> through
+        three years of growth, modernizing their WordPress theme to suit their
+        quickly growing product and content needs.
+      </p>
+
+      <div className="mt-16 mb-8 flex">
+        <Link href="https://pacifica.edu/" target="_blank">
           <Image className="w-24 invert" src={pacifica} alt="Pacifica" />
         </Link>
       </div>
-      <div className="mt-12 flex">
-        <Link href="https://usekojo.com" target="_blank">
+      <p>
+        <InlineLink href="https://www.pacifica.edu/">Pacifica</InlineLink> was
+        the first big website I worked on. I built key sections, including the
+        navigation, the faculty page, and article page in a custom WordPress
+        theme.
+      </p>
+
+      <div className="mt-16 mb-8 flex">
+        <Link href="https://www.usekojo.com/" target="_blank">
           <Kojo className="h-4 text-black" />
         </Link>
       </div>
-      <div className="mt-12 flex">
-        <Link href="https://jordyhelps.com" target="_blank">
-          <Jordyhelps className="h-7 text-black" />
+      <p>
+        I implemented a full rebrand for{" "}
+        <InlineLink href="https://www.usekojo.com/">Kojo</InlineLink> on their
+        Webflow website. I also built their custom pricing quiz in React and
+        implemented their web analytics and HubSpot forms.
+      </p>
+
+      <div className="mt-16 mb-8 flex">
+        <Link href="https://jordyhelps.com/" target="_blank">
+          <Jordyhelps className="h-6 text-black" />
         </Link>
       </div>
-      <div className="mt-12 flex">
-        <Link href="https://carlindunne.netlify.app" target="_blank">
+      <p>
+        My life coach,{" "}
+        <InlineLink href="https://jordyhelps.com/">Jordy</InlineLink>, is the
+        greatest person I know. I worked with{" "}
+        <InlineLink href="https://www.andrewantone.com/">
+          Andrew Antone
+        </InlineLink>{" "}
+        to design her brand and then built her a Next.js website.
+      </p>
+
+      <div className="mt-16 mb-8 flex">
+        <Link href="https://www.carlindunne5.com/" target="_blank">
           <Image className="w-8 grayscale" src={cd5} alt="CD5" />
         </Link>
       </div>
+      <p>
+        I am working with the{" "}
+        <InlineLink href="https://www.carlindunne5.com/">
+          Carlin Dunne Foundation
+        </InlineLink>{" "}
+        to build an application that helps users track their concussion
+        recovery. I took over their Gatsby project to help them reach
+        production.
+      </p>
+
       <hr />
+
       <p>My other side projects:</p>
+
       <Ul>
         <Li>
-          <InlineLink
-            href="https://github.com/joshuabradley012/joshbradley-hugo"
-            target="_blank"
-          >
+          <InlineLink href="https://github.com/joshuabradley012/joshbradley-hugo">
             Hugo theme (20 stars)
           </InlineLink>
         </Li>
         <Li>
-          <InlineLink
-            href="https://github.com/joshuabradley012/checkers"
-            target="_blank"
-          >
+          <InlineLink href="https://github.com/joshuabradley012/checkers">
             Checkers
           </InlineLink>
         </Li>
         <Li>
-          <InlineLink
-            href="https://github.com/joshuabradley012/tic-tac-toe"
-            target="_blank"
-          >
+          <InlineLink href="https://github.com/joshuabradley012/tic-tac-toe">
             Tic-tac-toe
           </InlineLink>
         </Li>
         <Li>
-          <InlineLink
-            href="https://github.com/joshuabradley012/mini-react"
-            target="_blank"
-          >
+          <InlineLink href="https://github.com/joshuabradley012/mini-react">
             Mini react
           </InlineLink>
         </Li>
         <Li>
-          <InlineLink
-            href="https://github.com/joshuabradley012/appfolio-searchapp"
-            target="_blank"
-          >
+          <InlineLink href="https://github.com/joshuabradley012/appfolio-searchapp">
             Appfolio scraper
           </InlineLink>
         </Li>
         <Li>
-          <InlineLink
-            href="https://github.com/joshuabradley012/tokenvalet"
-            target="_blank"
-          >
+          <InlineLink href="https://github.com/joshuabradley012/tokenvalet">
             Valet ticketing software
           </InlineLink>
         </Li>
--- components/ui/InlineLink.tsx
+++ components/ui/InlineLink.tsx
@@ -5,6 +5,13 @@ export default function InlineLink({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<typeof Link>) {
+  if (
+    !props.target &&
+    typeof props.href === "string" &&
+    !props.href.startsWith("/")
+  ) {
+    props.target = "_blank";
+  }
   return (
     <Link
       className={cn("underline transition-colors hover:text-black", className)}
